8c0f9c8cab80f4889e0ff2a8aa5947ac94548de8
AmaliMTZ <amtzschz4@gmail.com>
2026-04-14 12:42:37 -0600

    datos extra3

--- src/routes/payment.route.js
+++ src/routes/payment.route.js
@@ -1,12 +1,16 @@
-import { Router } from 'express';
-import { createOrder, success } from '../controllers/payment.controller.js';
+import { Router } from "express";
+import {
+  start3DSecure,
+  handle3DResponse,
+  createOrder,
+  success
+} from "../controllers/payment.controller.js";
 
 const router = Router();
 
-// Crear orden
-router.post('/create-order', createOrder);
-
-// Respuesta Banorte
-router.post('/success', success);
+router.post("/3d-secure", start3DSecure);
+router.post("/3d-response", handle3DResponse);
+router.get("/create-order", createOrder);
+router.post("/success", success);
 
 export default router;
