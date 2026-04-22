99569fa902d436d81995d170c594551ad86a2c45
AmaliMTZ <amtzschz4@gmail.com>
2026-04-22 14:10:03 -0600

    fix receipt import

--- src/services/receipt.service.js
+++ src/services/receipt.service.js
@@ -8,13 +8,22 @@ export const generateReceiptPDF = (res, data) => {
 
   doc.pipe(res);
 
+  // TÍTULO
   doc.fontSize(18).text("COMPROBANTE DE PAGO", { align: "center" });
   doc.moveDown();
 
+  // FECHA CORREGIDA A MÉXICO
+  const fecha = new Date().toLocaleString("es-MX", {
+    timeZone: "America/Mexico_City",
+    dateStyle: "short",
+    timeStyle: "medium"
+  });
+
+  // DATOS
   doc.fontSize(12);
   doc.text(`Orden: ${data.controlNumber}`);
   doc.text(`Monto: $${data.amount}`);
-  doc.text(`Fecha: ${new Date().toLocaleString()}`);
+  doc.text(`Fecha: ${fecha}`);
   doc.text(`Autorización: ${data.authCode}`);
   doc.text(`Referencia: ${data.reference}`);
 
