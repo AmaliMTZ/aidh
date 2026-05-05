16791b8be8e981f594c88ddf477062c1f46b5047
AmaliMTZ <amtzschz4@gmail.com>
2026-05-05 10:28:14 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ app.use((req, res, next) => {
     "max-age=31536000; includeSubDomains"
   );
   res.setHeader("X-Content-Type-Options", "nosniff");
-  res.setHeader("X-Frame-Options", "SAMEORIGIN");
+  
   next();
 });
 
